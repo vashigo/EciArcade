a8b5fe74e9a18423dcb1eea3c222605f69c3d355
Andres Vasquez <andresdavid.vasquez@gmail.com>
2019-11-21 19:48:25 -0500

    fix in verification email

--- src/EciArcade.py
+++ src/EciArcade.py
@@ -101,7 +101,9 @@ def ventana_autentificacion():
 
     def verifica():
         global ventana_selección
-        if re.match('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z0-9\_\-\.)]+\.[(a-z)]{2,15}$', correo.get().lower()):
+        #expresion regular para verificar los caracteres del correo
+        expRegular = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+        if re.match(expRegular, correo.get().lower()):
             if("mail.escuelaing.edu.co" in correo.get()):
                 if(vent_selec == 0):
                     ventana_selección()
